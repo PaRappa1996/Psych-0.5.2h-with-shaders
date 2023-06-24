0833aa72dcb67cb6963e39a3acabca57d1a3fbe3
PaRappa1996 <92826442+PaRappa1996@users.noreply.github.com>
2023-06-24 19:52:25 -0300

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -1150,7 +1150,7 @@ class BlockedGlitchEffect
     public var colorMultiplier(default, set):Float = 0;
     //public var hasColorTransform(default, set):Bool = false;
 
-    public function new(res:Float, time:Float, colorMultiplier:Float, /*colorTransform:Bool*/):Void
+    public function new(res:Float, time:Float, colorMultiplier:Float/*, colorTransform:Bool*/):Void
     {
         set_time(time);
         set_resolution(res);
